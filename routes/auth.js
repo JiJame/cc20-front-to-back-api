cec9052bd69033b64bad44d8cd72d71263de569e
JirapatPojjariyaporn1 <pojjariyapornjirapat@gmail.com>
2026-05-30 13:08:24 +0700

    init

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { register, login } from "../controllers/auth.js";
+// Controllers
+
+const router = express.Router();
+
+//ENDPOINT http://localhost:8000/auth/register
+router.post("/register", register);
+router.post("/login", login);
+
+// Export
+export default router;
