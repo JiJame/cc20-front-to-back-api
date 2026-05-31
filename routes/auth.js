2463e9cd5aeba8b7375611566d67cd7a7f8c1958
JirapatPojjariyaporn1 <pojjariyapornjirapat@gmail.com>
2026-05-31 10:56:43 +0700

    update register login

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { register, login } from "../controllers/auth.js";
 // Controllers
+import { register, login } from "../controllers/auth.js";
+import {
+  loginSchema,
+  registerSchema,
+  validateSchema,
+} from "../validations/validator.js";
 
 const router = express.Router();
 
 //ENDPOINT http://localhost:8000/auth/register
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateSchema(registerSchema), register);
+router.post("/login", validateSchema(loginSchema), login);
 
 // Export
 export default router;
